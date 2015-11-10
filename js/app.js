66dba13c2bc809534e36a50ab64c574dd1913736
Ying Dang <whydang@uw.edu>
2015-11-10 04:54:45 -0800

    completed audio visual :D

--- js/app.js
+++ js/app.js
@@ -68,7 +68,7 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
     return value < min ? min : value > max ? max : value;
   }
 
-  // analyser variables
+  // analyser variables, the internet is a great learning experience :D
   var canvas, ctx, source, context, analyser, fbc_array, bars, bar_x, bar_width, bar_height;
   
   function initializeMP3() {
@@ -84,17 +84,19 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
     frameLooper();
   }
 
+  var SAMPLES = 150;
+  var BAR_WIDTH = 3;
   function frameLooper() {
     window.requestAnimationFrame(frameLooper);
     fbc_array = new Uint8Array(analyser.frequencyBinCount);
     analyser.getByteFrequencyData(fbc_array);
     ctx.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
-    ctx.fillStyle = '#00CCFF'; // Color of the bars
-    bars = 100;
+    ctx.fillStyle = '#0066FF'; // Color of the bars
+    bars = SAMPLES;
     for (var i = 0; i < bars; i++) {
-      bar_x = i * 3;
-      bar_width = 2;
-      bar_height = (fbc_array[i] / 2);
+      bar_x = i * BAR_WIDTH;
+      bar_width = BAR_WIDTH;
+      bar_height = -(fbc_array[i] / 2);
       //  fillRect( x, y, width, height ) // Explanation of the parameters below
       ctx.fillRect(bar_x, canvas.height, bar_width, bar_height);
     }
