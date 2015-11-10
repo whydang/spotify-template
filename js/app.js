7827ea3e05f314af1588e7876d69399e0bf5763b
Ying Dang <whydang@uw.edu>
2015-11-10 02:19:09 -0800

    added changing image feature

--- js/app.js
+++ js/app.js
@@ -1,38 +1,48 @@
 var data;
-var baseUrl = 'https://api.spotify.com/v1/search?type=track&query='
-var baseUrlTwo = 'https://api.spotify.com/v1/artists/'
+var baseUrl = 'https://api.spotify.com/v1/search?type=track&query=';
+var defaultImage = 'img/spotify-logo.png';
 
-var myApp = angular.module('myApp', [])
+
+var myApp = angular.module('myApp', []);
 
 var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
-  $scope.audioObject = {}
+  $scope.submission = false;
+  $scope.displayImage = defaultImage;
+
+  $scope.audioObject = {};
   $scope.getSongs = function() {
+
+    $scope.submission = true;
+
     $http.get(baseUrl + $scope.track).success(function(response){
-      data = $scope.tracks = response.tracks.items
+      data = $scope.tracks = response.tracks.items;
       
     })
   }
 
-  $scope.play = function(song) {
+  // plays the track that was clicked on
+  $scope.play = function(track) {
+    var song = track.preview_url;
+
     if($scope.currentSong == song) {
-      $scope.audioObject.pause()
-      $scope.currentSong = false
-      return
+      $scope.audioObject.pause();
+      $scope.currentSong = false;
+      return;
     }
     else {
-      if($scope.audioObject.pause != undefined) $scope.audioObject.pause()
+      if($scope.audioObject.pause != undefined) $scope.audioObject.pause();
       $scope.audioObject = new Audio(song);
-      $scope.audioObject.play()  
-      $scope.currentSong = song
+      $scope.audioObject.play();
+      $scope.audioObject.loop=true;
+      $scope.currentSong = song;
+
+      // sets the image to be displayed each time another song is played
+      $scope.displayImage = track.album.images[0] ? track.album.images[0].url : defaultImage;
     }
   }
 
+  // determines whether or not the song should have play symbol next to it in row
   $scope.displayIcon = function(track) {
-    return ($scope.currentSong == track);
+    return ($scope.currentSong == track.preview_url);
   }
-})
-
-// Add tool tips to anything with a title property
-$('body').tooltip({
-    selector: '[title]'
 });
