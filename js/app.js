c92c770ebdf52d4a508b17b8d47d613d76b1f999
Ying Dang <whydang@uw.edu>
2015-11-10 19:48:00 -0800

    inputted cool scrolly features, UI friendly

--- js/app.js
+++ js/app.js
@@ -111,3 +111,22 @@ var myCtrl = myApp.controller('myCtrl', function($scope, $http) {
   }
 
 });
+
+// scrolls the page down to the search results
+myApp.directive('scrollOnClick', function() {
+  return {
+    restrict: 'A',
+    link: function(scope, $elm, attrs) {
+      var idToScroll = attrs.href;
+      $elm.on('click', function() {
+        var $target;
+        if (idToScroll) {
+          $target = $(idToScroll);
+        } else {
+          $target = $elm;
+        }
+        $("body").animate({scrollTop: $target.offset().top}, "slow");
+      });
+    }
+  }
+});
